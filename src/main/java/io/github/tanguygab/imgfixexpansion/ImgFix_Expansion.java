b9de20adc7b348d5fa41092414bb6e10679f8fbf
Tanguygab <tanguy@gabril.com>
2024-10-03 13:59:13 +0200

    Added Oraxen support with %imgfix_oraxen_:image:%

--- src/main/java/io/github/tanguygab/imgfixexpansion/ImgFix_Expansion.java
+++ src/main/java/io/github/tanguygab/imgfixexpansion/ImgFix_Expansion.java
@@ -5,6 +5,7 @@ import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import org.bukkit.OfflinePlayer;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -28,13 +29,23 @@ public final class ImgFix_Expansion extends PlaceholderExpansion {
     }
 
     @Override
+    public @NotNull List<String> getPlaceholders() {
+        return List.of("%imgfix_:image:%", "%imgfix_oraxen_:image:%");
+    }
+
+    @Override
     public String onRequest(OfflinePlayer player, @NotNull String params) {
+        boolean oraxen = false;
+        if (params.startsWith("oraxen_")) {
+            oraxen = true;
+            params = params.substring(7);
+        }
         params = PlaceholderAPI.setBracketPlaceholders(player,params);
         String output = params;
         Matcher m = pattern.matcher(params);
         while (m.find()) {
             String g = m.group();
-            output = output.replace(g,"{img_"+g.substring(1,g.length()-1)+"}");
+            output = output.replace(g,(oraxen ? "{oraxen_" : "{img_") +g.substring(1,g.length()-1)+"}");
         }
         return PlaceholderAPI.setBracketPlaceholders(player,output);
     }
