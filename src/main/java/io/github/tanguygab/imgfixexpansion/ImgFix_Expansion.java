b3d7eb2c89db3469aa35f90802d26ce61cbc7d2c
Tanguygab <tanguy@gabril.com>
2024-10-03 14:00:23 +0200

    1.0.1

--- src/main/java/io/github/tanguygab/imgfixexpansion/ImgFix_Expansion.java
+++ src/main/java/io/github/tanguygab/imgfixexpansion/ImgFix_Expansion.java
@@ -25,7 +25,7 @@ public final class ImgFix_Expansion extends PlaceholderExpansion {
 
     @Override
     public @NotNull String getVersion() {
-        return "1.0.0";
+        return "1.0.1";
     }
 
     @Override
